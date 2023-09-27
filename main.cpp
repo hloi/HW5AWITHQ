b98a9fd7788ae05df334bc4235b7624d879b6874
hloi <hloi@qcc.mass.edu>
2023-09-26 22:29:30 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "ProcessOrders.h"
+#include "Order.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ProcessOrders po;
+    double amount = po.process_order(2);
+    // shipment arrives.  Quantity of 5 items, cost $5 each
+    // inventory 5
+    amount = po.process_shipment(5,5);
+
+    // inventory 2 -> 7
+    amount = po.process_shipment(2,10);
+
+    amount = po.process_shipment(2,7);
+
+    amount = po.process_shipment(1,15);
+
+    amount = po.process_order(10);
+
     return 0;
 }
